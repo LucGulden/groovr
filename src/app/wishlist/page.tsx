56e714611b84b8a75f34eeecc8827e73f6599e59
LucGulden <luc.gulden@viacesi.fr>
2025-12-01 01:55:57 +0100

    Fix wishlist

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import dynamic from 'next/dynamic';
 import { useAuth } from '@/components/AuthProvider';
 import { useRouter } from 'next/navigation';
